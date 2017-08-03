65bd063572e30e70004a49d46439e9d939cc0db8
Jon <jon@dbs.org>
2017-08-03 15:55:26 -0500

    Added Server Info
    Temporary Route for K4

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/',     'HomeController@welcome')->name('welcome');
+Route::get('server-info', function () { phpinfo(); });
