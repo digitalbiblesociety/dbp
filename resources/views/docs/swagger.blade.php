58df91505464c85bf6f952c6a2b66c585c725bac
Jon <jon@dbs.org>
2017-10-19 18:06:07 -0500

    Added standalone swagger UI

--- resources/views/docs/swagger.blade.php
+++ resources/views/docs/swagger.blade.php
@@ -1,7 +1,58 @@
 @extends('layouts.app')
 
+@section('head')
+    <title>Swagger API</title>
+    <link href="/css/swagger-ui.css" rel="stylesheet" />
+    <style>
+        html
+        {
+            box-sizing: border-box;
+            overflow: -moz-scrollbars-vertical;
+            overflow-y: scroll;
+        }
+        *,
+        *:before,
+        *:after
+        {
+            box-sizing: inherit;
+        }
+
+        body {
+            margin:0;
+            background: #fafafa;
+        }
+    </style>
+@endsection
+
 @section('content')
 
     <div id="swagger-ui"></div>
 
+@endsection
+
+@section('footer')
+
+    <script src="/js/swagger-ui-bundle.js"> </script>
+    <script src="/js/swagger-ui-standalone-preset.js"> </script>
+    <script>
+        window.onload = function() {
+
+            // Build a system
+            const ui = SwaggerUIBundle({
+                url: "/swagger_v2.json",
+                dom_id: '#swagger-ui',
+                deepLinking: true,
+                presets: [
+                    SwaggerUIBundle.presets.apis,
+                    SwaggerUIStandalonePreset
+                ],
+                plugins: [
+                    SwaggerUIBundle.plugins.DownloadUrl
+                ],
+                layout: "StandaloneLayout"
+            })
+
+            window.ui = ui
+        }
+    </script>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,157 +1,170 @@
 @extends('layouts.app')
 
+<style>
+    small {
+        display: block
+    }
+    .card {
+        display: block;
+        background: #f8f8f8;
+        border:thin solid #ccc;
+        text-align: center;
+        padding:20px 15px;
+        margin:10px 0;
+        color:#222;
+    }
+    .card img {
+        width:25%;
+        float:left;
+    }
+
+    .card:hover {
+        background: #f0f0f0;
+    }
+    .title {
+        display: block;
+    }
+
+    .user-banner {
+        background: #333;
+        height:50px;
+        line-height:50px;
+    }
+    .user-banner a {
+        float: right;
+        padding:0 10px;
+    }
+
+    .user-banner .logo {
+        background: #f0f0f0;
+        width:50px;
+        height:50px;
+        border-radius: 100%;
+        position: relative;
+        margin:15px;
+        text-align: center;
+    }
+
+    .user-banner .logo .badge {
+        line-height:13px;
+        position: absolute;
+        bottom:-3px;
+        right:-3px;
+    }
+
+
+    .connections a {
+        display: inline-block;
+        line-height: 70px;
+        padding:5px;
+        color:#FFF;
+        text-shadow: 0 0 1px #000000;
+    }
+    .connections a.disabled {
+        opacity: .5;
+    }
+
+    .connections a img {
+        height:50px;
+        margin:10px;
+        float:left;
+    }
+
+    .github         {background-color:#181717}
+    .google         {background-color:#4285F4}
+    .bitbucket      {background-color:#205081}
+
+    .without-organization {
+        height:100%;
+    }
+    .without-organization div {
+        padding:20px;
+    }
+
+    .organization-join-panel {
+        height:100%;
+        background: #f1f1f1;
+        text-align: center;
+    }
+    .without-organization h2 {
+        font-size:1.75rem;
+    }
+
+</style>
 @section('head')
-    <style>
-        small {
-            display: block
-        }
-        .card {
-            display: block;
-            background: #f8f8f8;
-            border:thin solid #ccc;
-            text-align: center;
-            padding:20px 15px;
-            margin:10px 0;
-            color:#222;
-        }
-        .card img {
-            width:25%;
-            float:left;
-        }
-
-        .card:hover {
-            background: #f0f0f0;
-        }
-        .title {
-            display: block;
-        }
-
-        .user-banner {
-            background: #333;
-            height:50px;
-            line-height:50px;
-        }
-        .user-banner a {
-            float: right;
-            padding:0 10px;
-        }
-
-        .user-banner .logo {
-            background: #f0f0f0;
-            width:50px;
-            height:50px;
-            border-radius: 100%;
-            position: relative;
-            margin:15px;
-            text-align: center;
-        }
-
-        .user-banner .logo .badge {
-            line-height:13px;
-            position: absolute;
-            bottom:-3px;
-            right:-3px;
-        }
-
-
-        .connections a {
-            height: 70px;
-            display: block;
-            line-height: 70px;
-            color:#FFF;
-            text-shadow: 0 0 1px #000000;
-        }
-        .connections a.disabled {
-            opacity: .5;
-        }
-
-        .connections a img {
-            height:50px;
-            margin:10px;
-            float:left;
-        }
-
-        .github         {background-color:#181717}
-        .google         {background-color:#4285F4}
-        .bitbucket      {background-color:#205081}
-
-    </style>
 @endsection
 
 @section('content')
 
-    <nav class="user-banner">
-        <a class="logo" href="/user/{{ $user->id }}/alerts">A <span class="badge">4</span></a>
-    </nav>
+    @include('layouts.partials.banner', ['title' => "Hello $user->name" ])
 
-    <div class="row text-center">
-        <h1>Hello {{ $user->name }}</h1>
-        <p>You have 0 Alerts requiring your attention.</p>
-        <p>You are awaiting 5 replies from other organizations or users</p>
-    </div>
-
-    <div class="row">
-        <div class="medium-4 columns">
-            <h2>Access</h2>
-            <p>Your current organization status is:
-                @if(count($user->roles) > 0)
-                    @foreach($user->roles as $connection)
-                        <a href="{{ route('dashboard_organizations.show',['id' => $connection->organization->id]) }}" class="card">
-                            <img src="{{ $connection->organization->logoIcon->icon or $connection->organization->logo->url }}" />
-                            <span class="title">{{ $connection->organization->translations("eng")->first()->name }}</span>
-                            <small class="subtitle">{{ $connection->role }}</small>
-                            @if(($connection->role == "manager") | ($connection->role == "admin"))
-                                <a href=""></a>
-                            @endif
-                        </a>
-                    @endforeach
-                    <a href="{{ route('dashboard_organization_roles.create') }}" class="button">Add an Organization</a>
-                @else
-                    <strong>NOT SET?! <a href="{{ route('dashboard_organization_roles.create') }}" class="button">Request to join one now!</a></strong>
+    @if(count($user->roles) > 0)
+        @foreach($user->roles as $connection)
+            <a href="{{ route('dashboard_organizations.show',['id' => $connection->organization->id]) }}" class="card">
+                <img src="{{ $connection->organization->logoIcon->icon or $connection->organization->logo->url }}" />
+                <span class="title">{{ $connection->organization->translations("eng")->first()->name }}</span>
+                <small class="subtitle">{{ $connection->role }}</small>
+                @if(($connection->role == "manager") | ($connection->role == "admin"))
+                    <a href=""></a>
                 @endif
-            </p>
+            </a>
+        @endforeach
+        <a href="{{ route('dashboard_organization_roles.create') }}" class="button">Add an Organization</a>
+
+        <div class="row">
+
+            <div class="medium-4 columns">
+                <h2>Create new Resources</h2>
+                <ul>
+                    @if($user->roles->where('organization', '!=', null)->where('role','!=','requesting-access')->first())
+                        <li><a href="/bibles/create">Create Bible</a></li>
+                        <li><a href="/resources/create">Create Resource</a></li>
+                    @else
+                        <li><span class="disabled">Create Bible</span></li>
+                        <li><span class="disabled">Create Resource</span></li>
+                        <small class="text-center">You need Organization Permissions to Edit Bible or Resource Data</small>
+                    @endif
+                    @if($user->archivist)
+                        <li><a href="/languages/create">Create Language</a></li>
+                        <li><a href="/alphabets/create">Create Alphabet</a></li>
+                        <li><a href="/countries/create">Create Country</a></li>
+                    @else
+                        <li><span class="disabled">Create Language</span></li>
+                        <li><span class="disabled">Create Alphabet</span></li>
+                        <li><span class="disabled">Create Country</span></li>
+                        <small class="text-center">You need Archivist Permissions to Edit Language, Country, or Alphabet Data</small>
+                    @endif
+                </ul>
+            </div>
         </div>
-        <div class="medium-4 columns">
-            <h2>Connected Accounts</h2>
-            <p>Your current connected accounts are:
-                <div class="organization connections">
-                    <a href="https://github.com/" class="github @if(!$user->github) disabled @endif"><img src="/img/social/github.svg" /> Github</a>
-                    <a href="https://google.com/" class="google @if(!$user->google) disabled @endif"><img src="/img/social/google.svg" /> Google</a>
-                    <a href="https://bitbucket.org/" class="bitbucket @if(!$user->bitbucket) disabled @endif"><img src="/img/social/bitbucket.svg" /> Bitbucket</a>
+
+    @else
+        <div class="row expanded without-organization">
+            <div class="medium-6 columns organization-join-panel">
+                <h2>Your account is not associated with an organization</h2>
+                <div class="medium-8 columns centered">
+                <p>If you want to do things like add new Bibles or edit meta data information you'll need to join one</p>
+                <a href="{{ route('dashboard_organization_roles.create') }}" class="button expanded">Request to join one now!</a>
                 </div>
-            </p>
-        </div>
-        <div class="medium-4 columns">
-            <h2>Documentation</h2>
-            <p>Read it, there's plenty to learn</p>
-            <a class="button" href="/docs">Learn More</a>
-        </div>
-    </div>
-    <div class="row">
+                <svg xmlns="http://www.w3.org/2000/svg" width="163.1" height="151.7">
+                    <g transform="translate(-747.63 -930.078)">
+                        <path d="M772.5 976.8c-4.7 0-8.5 3.8-8.5 8.4 0 4.7 3.8 8.5 8.5 8.5 4.6 0 8.4-3.8 8.4-8.5 0-4.6-4-8.4-8.5-8.4zm-8 16.4c-2.4.5-4 2.5-4 5v13c0 2.5 1.6 4.6 4 5h.2v16.3c0 1.5 1.2 2.8 2.7 2.8h10.2c1.5 0 2.7-1.3 2.7-2.8v-16.2h.2c2.4-.5 4.2-2.6 4.2-5v-13c0-2.6-1.8-4.6-4-5-2 2.3-5 3.8-8.2 3.8-3.2 0-6-1.4-8-3.8zM865.2 988.2c-5.2 0-9.4 4.2-9.4 9.4s4.2 9.5 9.4 9.5 9.5-4.2 9.5-9.4-4.3-9.4-9.5-9.4zm-9 18.4c-2.5.5-4.5 2.8-4.5 5.6v14.5c0 2.8 2 5 4.6 5.7h.3v18c0 1.8 1.3 3.2 3 3.2H871c1.6 0 3-1.4 3-3v-18.2h.2c2.7-.6 4.6-3 4.6-5.7v-14.5c0-2.8-2-5-4.5-5.6-2 2.6-5.4 4.3-9 4.3-3.6 0-6.8-1.8-9-4.4z" fill="#999" overflow="visible"/>
+                        <path d="M814.6 981.6c-6.2 0-11.4 5-11.4 11.4 0 6.2 5.2 11.3 11.4 11.3 6.3 0 11.4-5 11.4-11.3 0-6.3-5-11.4-11.4-11.4zm-10.7 22c-3.3.8-5.6 3.5-5.6 7v17.3c0 3.3 2.3 6 5.5 6.7h.2v22c0 2 1.6 3.5 3.7 3.5h13.5c2 0 3.7-1.6 3.7-3.6v-22h.2c0 .2 0 0 0 0 3.3-.5 5.6-3.3 5.6-6.7v-17.5c0-3.4-2.3-6-5.5-6.8-2.5 3.2-6.4 5.3-10.8 5.3s-8.2-2-10.7-5.3z" fill="#00b09b" overflow="visible"/>
+                        <path d="M843.3 957.3c-3.7 0-6.8 3-6.8 7 0 3.6 3 6.7 6.8 6.7s7-3 7-6.8-3.2-7-7-7zm-6.4 13.3c-2 .4-3.4 2-3.4 4v10.6c0 2 1.4 3.8 3.3 4v13.3c0 1.2 1 2.2 2.3 2.2h8.2c1.2 0 2.2-1 2.2-2.2v-13.2h.2c1.8-.3 3.2-2 3.2-4v-10.6c0-2-1.4-3.7-3.3-4-1.6 2-4 3-6.6 3-2.7 0-5-1-6.5-3zM888.3 968.6c-3.3 0-6 2.7-6 6s2.7 6 6 6 6-2.7 6-6-2.7-6-6-6zm-5.7 11.7c-1.6.3-3 1.8-3 3.6v9c0 1.8 1.4 3.3 3 3.6h.2v11.6c0 1 .8 2 2 2h7c1.2 0 2-1 2-2v-11.6h.2c1.7-.3 3-1.8 3-3.5v-9c0-2-1.3-3.4-3-3.7-1.3 1.7-3.4 2.8-5.7 2.8-2.3 0-4.3-1-5.6-2.7zM793.4 962.4c-2.8 0-5 2.3-5 5 0 3 2.2 5.2 5 5.2s5-2.3 5-5c0-3-2.2-5.2-5-5.2zm-4.8 10c-1.5.2-2.5 1.4-2.5 3v7.7c0 1.6 1 3 2.6 3.2v9.8c0 1 .8 1.6 1.7 1.6h6.2c1 0 1.6-.7 1.6-1.6v-9.8h.2c1.4-.3 2.5-1.6 2.5-3v-8c0-1.4-1-2.6-2.5-3-1 1.5-2.8 2.4-4.8 2.4s-3.7-1-4.8-2.3z" fill="#999" overflow="visible"/>
+                    </g>
+                </svg>
 
-        <div class="medium-4 columns">
-            <h2>Create new Resources</h2>
-            <ul>
-                @if($user->roles->where('organization', '!=', null)->where('role','!=','requesting-access')->first())
-                    <li><a href="/bibles/create">Create Bible</a></li>
-                    <li><a href="/resources/create">Create Resource</a></li>
-                @else
-                    <li><span class="disabled">Create Bible</span></li>
-                    <li><span class="disabled">Create Resource</span></li>
-                    <small class="text-center">You need Organization Permissions to Edit Bible or Resource Data</small>
-                @endif
-                @if($user->archivist)
-                    <li><a href="/languages/create">Create Language</a></li>
-                    <li><a href="/alphabets/create">Create Alphabet</a></li>
-                    <li><a href="/countries/create">Create Country</a></li>
-                @else
-                    <li><span class="disabled">Create Language</span></li>
-                    <li><span class="disabled">Create Alphabet</span></li>
-                    <li><span class="disabled">Create Country</span></li>
-                    <small class="text-center">You need Archivist Permissions to Edit Language, Country, or Alphabet Data</small>
-                @endif
-            </ul>
+            </div>
+            <div class="medium-6 columns text-center">
+                <h2>You can query the API or Read Documentation</h2>
+                <div class="medium-8 columns centered">
+                <p>You don't need an official organization to query information about Bibles.</p>
+                <a href="{{ route('swagger') }}" class="button expanded">Docs!</a>
+                </div>
+            </div>
         </div>
-    </div>
+    @endif
+
+
 
 @endsection
