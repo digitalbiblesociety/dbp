d16cd1540d2de4574f9ef8f8b881078d83d276d0
Jon <jon@dbs.org>
2017-11-11 18:25:32 -0600

    API Controller cruft removal

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -50,16 +50,6 @@ class APIController extends Controller
         return $this->statusCode;
     }
 
-    public function getVerse($abbr,$reference)
-    {
-        $id = \DB::connection('sophia')->table('bible_list')->where('fcbhId',$abbr)->first();
-        if(isset($id)) {
-            $verse = \DB::connection('sophia')->table(str_replace('-','_',$id->translationId).'_vpl')->where('verseID',$reference)->first();
-            return $verse;
-        }
-        return NULL;
-    }
-
     /**
      * Set Status Code
      *
@@ -72,12 +62,6 @@ class APIController extends Controller
         return $this;
     }
 
-    public function setIsoPreference($isoPreference)
-    {
-        $this->isoPreference = $isoPreference;
-        return $this;
-    }
-
     /**
      *
      * Get The object and return it in the format requested via query params.
@@ -87,6 +71,7 @@ class APIController extends Controller
      */
     public function reply($object, $transformer = null, $key = 0, $pretty = 0)
     {
+    	if(isset($_GET['echo'])) $object = [$_GET,$object];
         if (isset($_GET['key'])) {
             $key = \DB::table('users')->where('id','=',$_GET['key'])->first();
             if(!$key) return $this->setStatusCode(403)->replyWithError('Provided authentication key does not match a key in our records');
@@ -118,58 +103,12 @@ class APIController extends Controller
     }
 
     /**
-     *
-     * Fetches Json Feed from external API and saves it to storage
-     *
-     * @param $url
-     * @param $name
-     * @return mixed|string
-     */
-    public function saveJson($url, $name)
-    {
-
-        $opts = array(
-            'http' => array(
-                'method' => "GET",
-                'header' => "Accept-language: en\r\n"
-            )
-        );
-
-        $context = stream_context_create($opts);
-        $json = file_get_contents($url, false, $context);
-        file_put_contents(storage_path() . '/data/' . $name . '.json', $json);
-        $json = json_decode($json, true);
-        return $json;
-    }
-
-    /**
-     * @param $url
-     * @param $name
-     * @return mixed|string
-     */
-    public function fetchJson($url, $name)
-    {
-        $opts = array(
-            'http' => array(
-                'method' => "GET",
-                'header' => "Accept-language: en\r\n"
-            )
-        );
-        $context = stream_context_create($opts);
-        $json = file_get_contents($url, false, $context);
-        $json = json_decode($json, true);
-        return $json;
-    }
-
-    /**
      * @param $message
      * @return mixed
      */
     public function replyWithError($message)
     {
         $status = $this->getStatusCode();
-
-
 	    if(!$this->api AND !isset($status)) return view('errors.broken',compact('message'));
 	    if(!$this->api) return view("errors.$status",compact('message','status'));
 
@@ -177,61 +116,6 @@ class APIController extends Controller
             'message' => $message,
             'status code' => $status
         ]], $this->getStatusCode(), array(), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)->header('Content-Type', 'text/json');
-    }
-
-    public function replyWithErrorView($message)
-    {
-        $status = $this->getStatusCode();
-        return view('errors.broken', compact('message','status'));
-    }
-
-
-    /**
-     * Removes undesirable keys from array elements
-     *
-     * @param array $flattened
-     * @param array $children
-     * @return array
-     */
-    public function flattenChildren(array $flattened, array $children) {
-        foreach($flattened as $key => $value) {
-            foreach($children as $child) {
-                if(isset($value[$child])) {
-                    $flattened[$key][$child] = array_flatten($flattened[$key][$child]);
-                }
-            }
-        }
-        return $flattened;
-    }
-
-
-    public function vernacular_numbers($number) {
-
-        switch(i18n::getCurrentLocale()) {
-            case 'ara':
-                $number = number_format($number);
-                $arabic_eastern = array('٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩');
-                $arabic_western = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
-                $number = str_replace($arabic_western, $arabic_eastern, $number);
-                return $number;
-                break;
-        }
-    }
-
-    public function transformDataTable($objects,$name,$primary,$primaryTitle = null)
-    {
-        foreach($objects as $key => $object) {
-            if(isset($primaryTitle)) {
-                $object[$primary] = '<a href="/'.$name.'/'.$object[$primary].'">'.$object[$primaryTitle].'</a>';
-                unset($object[$primaryTitle]);
-            } else {
-                $object[$primary] = '<a href="/'.$name.'/'.$object[$primary].'">'.$object[$primary].'</a>';
-            }
-
-            $objects[$key] = array_flatten($object);
-        }
-        return ['data' => $objects];
-
     }
 
 }
