5e486303cff002a5b09a7d955ab50111b9499529
Jon <jon@dbs.org>
2017-11-12 03:07:00 -0600

    Increase Rate Limit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
 
         'api' => [
 	        \Barryvdh\Cors\HandleCors::class,
-            'throttle:60,1',
+            'throttle:120,1',
             'bindings',
         ],
     ];
