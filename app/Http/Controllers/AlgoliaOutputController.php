0f871a68999a7a0f18bf55709b352aef11fff64e
Jon <jon@dbs.org>
2018-07-30 17:26:19 -0500

    Typo Correction

--- app/Http/Controllers/AlgoliaOutputController.php
+++ app/Http/Controllers/AlgoliaOutputController.php
@@ -21,7 +21,7 @@ class AlgoliaOutputController extends APIController
     {
 	    if (env('APP_ENV') == 'local') ini_set('memory_limit', '864M');
 		$languages = Language::with(['classifications', 'translations', 'countries', 'region'])->get();
-		return $this->reply(fractal($languages, new AlgoliaTransformer());
+		return $this->reply(fractal($languages, new AlgoliaTransformer()));
     }
 
 
