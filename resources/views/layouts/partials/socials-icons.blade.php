0344c69f9cbb002fd241d65837c23b77ab04ebbb
Jon <jon@dbs.org>
2018-08-06 16:56:18 -0500

    Merge social partials

--- resources/views/layouts/partials/socials-icons.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-    <div class="col-12 mb-2 text-center">
-        <a href="{{ route('social.redirect',['provider' => 'facebook']) }}">Facebook</a> |
-        <a href="{{ route('social.redirect',['provider' => 'github']) }}">Github</a> |
-        <a href="{{ route('social.redirect',['provider' => 'google']) }}">Google</a> |
-        <a href="{{ route('social.redirect',['provider' => 'twitter']) }}">Twitter</a>
-    </div>
-</div>
