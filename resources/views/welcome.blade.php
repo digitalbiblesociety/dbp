fb6d98eaf181bd83c9b80b06b01eb635572cd994
Jon <jon@dbs.org>
2018-08-15 01:19:35 -0500

    Remove Template banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,11 +29,7 @@
         </div>
     </div>
 </section>
-<div class="box cta">
-    <p class="has-text-centered">
-        <span class="tag is-primary">New</span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    </p>
-</div>
+
 <section class="container">
     <div class="columns features">
 
