62cb6f7aa7b4c38459ae38a525def6488e4e20ae
Jon <jon@dbs.org>
2018-08-16 22:47:44 -0500

    Added breadcrumbs to banner

--- resources/views/layouts/partials/banner.blade.php
+++ resources/views/layouts/partials/banner.blade.php
@@ -1,33 +1,32 @@
 <section class="hero is-primary">
-    <div class="hero-head">
-        @if(isset($breadcrumbs))
-        <nav class="breadcrumb" aria-label="breadcrumbs">
-            <ul>
-                @foreach($breadcrumbs as $url => $breadcrumb)
-                    @if($url != "#")
-                    <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-                        <a itemprop="item" href="{{ $url }}">
-                            <span itemprop="name">{{ $breadcrumb }}</span>
-                            <meta itemprop="position" content="{{ $loop->iteration }}" />
-                        </a>
-                    </li>
-                    @else
-                        <li class="is-active" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-                            <span itemprop="item" aria-current="page">
-                                <span itemprop="name">{{ $breadcrumb }}</span>
-                                <meta itemprop="position" content="{{ $loop->iteration }}" />
-                            </span>
-                        </li>
-                    @endif
-                @endforeach
-            </ul>
-        </nav>
-        @endif
-    </div>
     <div class="hero-body">
         <div class="container">
             <h1 class="title">{{ $title }}</h1>
             @if(isset($subtitle)) <h2 class="subtitle">{{ $subtitle }}</h2> @endif
+
+            @if(isset($breadcrumbs))
+                <nav class="breadcrumb has-dot-separator" aria-label="breadcrumbs">
+                    <ul>
+                        @foreach($breadcrumbs as $url => $breadcrumb)
+                            @if($url != "#")
+                                <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                                    <a itemprop="item" href="{{ $url }}">
+                                        <span itemprop="name">{{ $breadcrumb }}</span>
+                                        <meta itemprop="position" content="{{ $loop->iteration }}" />
+                                    </a>
+                                </li>
+                            @else
+                                <li class="is-active" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                                    <a itemprop="item" aria-current="page" href="#">
+                                        <span itemprop="name">{{ $breadcrumb }}</span>
+                                        <meta itemprop="position" content="{{ $loop->iteration }}" />
+                                    </a>
+                                </li>
+                            @endif
+                        @endforeach
+                    </ul>
+                </nav>
+            @endif
         </div>
     </div>
 </section>
