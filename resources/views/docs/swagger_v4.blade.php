2a3d1c4bb6ffa8d7d505f78b867b79fa8664a4ae
Jon <jon@dbs.org>
2018-08-20 12:59:04 -0500

    Documetation extends layouts.app

--- resources/views/docs/swagger_v4.blade.php
+++ resources/views/docs/swagger_v4.blade.php
@@ -1,24 +1,19 @@
-@extends('layouts.simple')
+@extends('layouts.app')
 
 @section('head')
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <style>html, body {height: 100%}
-
-    .openapi {
-        width: 95%;
-        height: 100%;
-        margin: 0 auto;
-    }
+    <style>
+        html, body {height: 100%}
+        .openapi {
+            width:90%;
+            margin:0 auto;
+        }
     </style>
 @endsection
 
 @section('content')
-    <div class="row">
-        <div id="app"></div>
-    </div>
 @endsection
 
-@section('footer_scripts')
+@section('footer')
     <script src="/js/swagger-vue-v4.js"></script>
 @endsection
