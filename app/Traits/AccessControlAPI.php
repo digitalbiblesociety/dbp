8216426fea644508c89bdb1dbbf1395efe8910fc
Jon <jon@dbs.org>
2018-08-24 10:45:54 -0500

    Query Access control by access type id

--- app/Traits/AccessControlAPI.php
+++ app/Traits/AccessControlAPI.php
@@ -3,6 +3,7 @@
 namespace App\Traits;
 
 use App\Models\User\AccessGroup;
+use App\Models\User\AccessType;
 
 trait AccessControlAPI {
 
@@ -22,14 +23,16 @@ trait AccessControlAPI {
 		if(!isset($user_location->iso_code)) $user_location->iso_code = "unset";
 		if(!isset($user_location->continent)) $user_location->continent = "unset";
 
+		$access_type = AccessType::where('name',$type)->first();
+
 		$access = [];
 		$accessGroups = AccessGroup::with('filesets')
-			->whereHas('types', function ($query) use ($user_location,$type) {
+			->whereHas('types', function ($query) use ($user_location,$access_type) {
 				$query->where(function($query) use ($user_location) {
 					$query->where('country_id', $user_location->iso_code)->orWhere('country_id', '=', null);
 				})->where(function($query) use ($user_location) {
 					$query->where('country_id', $user_location->continent)->orWhere('continent_id', '=', null);
-				})->where('name',$type);
+				})->where('access_type_id', $access_type->id);
 			})->whereHas('keys', function ($query) use ($api_key) {
 				$query->where('key_id', $api_key);
 			})->get();
