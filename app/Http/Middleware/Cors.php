a07ee36813782142ec5001a2fd39a1ef24a85216
Jon <jon@dbs.org>
2018-08-30 13:44:28 -0500

    Preflight options method fix for chrome

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,6 +15,14 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin', '*')->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	    if ($request->isMethod('OPTIONS')) {
+		    app()->options($request->path(), function() { return response('', 200); });
+	    }
+
+	    $response = $next($request);
+	    $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
+	    $response->header('Access-Control-Allow-Headers', '*');
+	    $response->header('Access-Control-Allow-Origin', '*');
+	    return $response;
     }
 }
