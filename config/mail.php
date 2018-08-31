a9292c1ad01248ac3c7d25dc381b5a45706b5dbc
Jon <jon@dbs.org>
2018-08-31 17:07:16 -0500

    default to dbp4.org

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@dbp4.org'),
         'name'    => env('MAIL_FROM_NAME', 'Example'),
     ],
 
