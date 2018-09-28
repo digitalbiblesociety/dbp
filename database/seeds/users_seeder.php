a935bc32273ca8f64cbad76c8fbc53b5c2b73a46
Jon <jon@dbs.org>
2018-09-28 15:30:36 -0500

    User Seeder

--- database/seeds/users_seeder.php
+++ database/seeds/users_seeder.php
@@ -3,11 +3,14 @@
 use Illuminate\Database\Seeder;
 use App\Models\User\User;
 
-
+use App\Models\Bible\BibleFileset;
+use App\Models\Bible\Book;
 use App\Models\User\Study\HighlightColor;
 use App\Models\User\Study\Highlight;
 use App\Models\User\Study\Note;
 use App\Models\User\Study\Bookmark;
+use Carbon\Carbon;
+
 class users_seeder extends Seeder
 {
     /**
@@ -18,6 +21,10 @@ class users_seeder extends Seeder
     public function run()
     {
     	$this->seedUsers();
+	    $this->seedBookmarks();
+	    $this->seedAccounts();
+	    $this->seedNotes();
+	    $this->seedHighlights();
     }
 
 
@@ -31,7 +38,7 @@ class users_seeder extends Seeder
 			    $currentUser = [
 				    'id'                => $user->id,
 				    'name'              => $user->username ?? $user->email,
-				    'password'          => 's_'.bcrypt($user->password),
+				    'password'          => bcrypt($user->password),
 				    'first_name'        => $user->first_name,
 				    'last_name'         => $user->last_name,
 				    'token'             => str_random(24),
@@ -52,23 +59,44 @@ class users_seeder extends Seeder
 	    $filesets = BibleFileset::all();
 	    $skippedFilesets = [];
 
+	    $books = Book::select(['id_osis','id_usfx','id'])->get();
+
 	    \DB::connection('dbp_users_v2')
 	       ->table('highlight')->where('id','>',$first_highlight_id)->orderBy('id')->where('status','current')
-	       ->chunk(500, function ($highlights) use($filesets, $skippedFilesets, $colorEquivalents) {
+	       ->chunk(500, function ($highlights) use($filesets, $skippedFilesets, $colorEquivalents, $books) {
 		    foreach($highlights as $highlight) {
-		    	if(in_array($highlight->dam_id, $skippedFilesets)) { continue; }
-			    $fileset = $filesets->where('id', $highlight->dam_id)->orWhere('id',substr($highlight->dam_id,0,-4))->orWhere('id',substr($highlight->dam_id,0,-2))->first();
-		    	if(!$fileset) {$skippedFilesets[] = $highlight->dam_id;continue;}
+			    $fileset = $filesets->where('id', $highlight->dam_id)->first();
+			    if(!$fileset) $fileset = $filesets->where('id',substr($highlight->dam_id,0,-4))->first();
+			    if(!$fileset) $fileset = $filesets->where('id',substr($highlight->dam_id,0,-2))->first();
+			    if(!$fileset) {continue;}
+			    if($fileset->bible->first()) {
+				    if(!isset($fileset->bible->first()->id)) {continue;}
+			    } else {
+				    continue;
+			    }
+
+			    $book = $books->where('id_osis', $highlight->book_id)->first();
+			    if(!isset($book)) {
+			    	echo "\n Missing Book: ". $highlight->book_id;
+			    	continue;
+			    }
 
 		    	$verse = \DB::connection('sophia')
-			                ->table($fileset->id.'_vpl')->select('verse_text')
-			                ->where('chapter',$highlight->chapter)->where('verse',$highlight->verse_start)->first();
+			                ->table($fileset->id.'_vpl')->select(['verse_text','chapter'])->where('book',$book->id_usfx)
+			                ->where('chapter',$highlight->chapter_id)->where('verse_start',$highlight->verse_id)->first();
+		    	if(!isset($verse)) {
+		    		echo "\nMissing Fileset: " . $fileset->id.'_vpl';
+		    		break;
+			    }
 		    	$word_count = count(explode(' ',$verse->verse_text));
 
 				Highlight::create([
 					'id'                => $highlight->id,
 					'user_id'           => $highlight->user_id,
 					'fileset_id'        => $fileset->id,
+					'book_id'           => $book->id,
+					'chapter'           => $highlight->chapter_id,
+					'verse_start'       => $highlight->verse_id,
 					'highlight_start'   => 1,
 					'highlighted_words' => $word_count,
 					'highlighted_color' => $colorEquivalents[$highlight->color]
@@ -84,17 +112,27 @@ class users_seeder extends Seeder
 		$first_note = \App\Models\User\Study\Note::orderBy('id','DESC')->first();
 		$first_note_id = $first_note->id ?? 0;
 
+		$skippedFilesets = [];
 		$filesets = BibleFileset::with('bible')->get();
 		$books = Book::select('id_osis','id')->get()->pluck('id','id_osis')->toArray();
 
-		\DB::connection('dbp_users_v2')->table('note')->where('id','>',$first_note_id)->orderBy('id')->chunk(500, function ($notes) use($filesets, $books) {
+		\DB::connection('dbp_users_v2')->table('note')->where('id','>',$first_note_id)->orderBy('id')->chunk(500, function ($notes) use($filesets, $books, $skippedFilesets) {
 			foreach($notes as $note) {
-				$fileset = $filesets->where('id', $note->dam_id)->orWhere('id',substr($note->dam_id,0,-4))->orWhere('id',substr($note->dam_id,0,-2))->first();
-				if(!$fileset) {$skippedFilesets[] = $note->dam_id; continue;}
+				$fileset = $filesets->where('id', $note->dam_id)->first();
+				if(!$fileset) $fileset = $filesets->where('id',substr($note->dam_id,0,-4))->first();
+				if(!$fileset) $fileset = $filesets->where('id',substr($note->dam_id,0,-2))->first();
+				if(!$fileset) {continue;}
+				if($fileset->bible->first()) {
+					if(!isset($fileset->bible->first()->id)) {continue;}
+				} else {
+					continue;
+				}
+				if(!isset($books[$note->book_id])) {continue;}
 
 				Note::create([
+					"id"            => $note->id,
 					"user_id"       => $note->user_id,
-					"bible_id"      => $fileset->bible->id,             //  => "ENGESVO2ET"
+					"bible_id"      => $fileset->bible->first()->id,             //  => "ENGESVO2ET"
 					"book_id"       => $books[$note->book_id],          //  => "Ezra"
 					"chapter"       => $note->chapter_id,               //  => "1"
 					"verse_start"   => $note->verse_id,                 //  => "1"
@@ -104,6 +142,7 @@ class users_seeder extends Seeder
 				]);
 			}
 		});
+		file_put_contents(storage_path('/data/dbp2_seed_logs_highlights.json'),json_encode([$skippedFilesets]));
 	}
 
 	public function seedBookmarks()
@@ -113,23 +152,38 @@ class users_seeder extends Seeder
 
 		$filesets = BibleFileset::with('bible')->get();
 		$books = Book::select('id_osis','id')->get()->pluck('id','id_osis')->toArray();
+		$skippedFilesets = [];
+		$skippedBooks = [];
 
 		\DB::connection('dbp_users_v2')->table('bookmark')
 			->where('status','current')->where('id','>',$first_bookmark_id)
-			->orderBy('id')->chunk(500, function ($bookmarks) use($filesets, $books) {
+			->orderBy('id')->chunk(500, function ($bookmarks) use($filesets, $books, $skippedFilesets, $skippedBooks) {
 
 				foreach ($bookmarks as $bookmark) {
+
+					$fileset = $filesets->where('id', $bookmark->dam_id)->first();
+					if(!$fileset) $fileset = $filesets->where('id',substr($bookmark->dam_id,0,-4))->first();
+					if(!$fileset) $fileset = $filesets->where('id',substr($bookmark->dam_id,0,-2))->first();
+					if(!$fileset) {$skippedFilesets[] = $bookmark->dam_id; continue;}
+					if(!isset($fileset->bible)) {$skippedFilesets[] = $bookmark->dam_id; continue;}
+
+					if(!isset($books[$bookmark->book_id])) {$skippedBooks[] = $bookmark->book_id;continue;}
+
 					Bookmark::create([
+						'id'         => $bookmark->id,
 						'user_id'    => $bookmark->user_id,
-						'bible_id'   => $bookmark->bible_id,
+						'bible_id'   => $fileset->bible->first()->id,
 						'book_id'    => $books[$bookmark->book_id],
-						'chapter_id' => $bookmark->chapter_id,
-						'verse_id'   => $bookmark->verse_start,
+						'chapter'    => $bookmark->chapter_id,
+						'verse_start'=> $bookmark->verse_id,
 						'created_at' => Carbon::createFromTimeString($bookmark->created)->toDateString(),
 						'updated_at' => Carbon::createFromTimeString($bookmark->updated)->toDateString()
 					]);
+
 				}
 		});
+
+		file_put_contents(storage_path('/data/dbp2_seed_logs_bookmarks.json'),json_encode(['filesets' => $skippedFilesets,'books'=>$skippedBooks]));
 	}
 
 	public function seedProfiles()
@@ -140,21 +194,28 @@ class users_seeder extends Seeder
 	public function seedAccounts()
 	{
 
-		\DB::connection('dbp_users_v2')->table('remote')
-		   ->where('status','current')->where('id','>',$first_account_id)
-		   ->orderBy('id')->chunk(500, function ($bookmarks) use($filesets, $books) {
+		$first_account = \App\Models\User\Account::orderBy('id','DESC')->first();
+		$first_account_id = $first_account->id ?? 0;
 
-				foreach ($bookmarks as $bookmark) {
-					Bookmark::create([
-						'user_id'    => $bookmark->user_id,
-						'bible_id'   => $bookmark->bible_id,
-						'book_id'    => $books[$bookmark->book_id],
-						'chapter_id' => $bookmark->chapter_id,
-						'verse_id'   => $bookmark->verse_start,
-						'created_at' => Carbon::createFromTimeString($bookmark->created)->toDateString(),
-						'updated_at' => Carbon::createFromTimeString($bookmark->updated)->toDateString()
-					]);
-				}
+		\DB::connection('dbp_users_v2')->table('user_remote')
+			->select(['id','user_id','remote_id','remote_type'])->where('id','>',$first_account_id)
+			->orderBy('id')->distinct()->chunk(500, function ($accounts) {
+
+		   	    foreach ($accounts as $account) {
+
+		   	    	if(\App\Models\User\Account::where([
+				        'user_id'          => $account->user_id,
+				        'provider_id'      => $account->remote_type
+			        ])->exists()) {continue;}
+
+				    \App\Models\User\Account::create([
+					    'id'               => $account->id,
+					    'user_id'          => $account->user_id,
+					    'provider_user_id' => $account->remote_id,
+					    'provider_id'      => $account->remote_type
+				    ]);
+		        }
+
 			});
 
 	}
