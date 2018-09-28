b7c2b126b00640bc5bbdb5fb9308b4d93ff2935e
Jon <jon@dbs.org>
2018-09-28 14:59:40 -0500

    Added Github deployment route

--- routes/api.php
+++ routes/api.php
@@ -180,3 +180,6 @@
 	Route::name('v4_articles.update')->put('articles/{article_id}',                        'User\ArticlesController@update');
 	Route::name('v4_articles.store')->post('articles',                                     'User\ArticlesController@store');
 	Route::name('v4_articles.destroy')->delete('articles/{article_id}',                    'User\ArticlesController@destroy');
+
+	// VERSION 4 | DEPLOYMENT
+	Route::post('/deploy/github',                                                          'DeployController@deploy');
