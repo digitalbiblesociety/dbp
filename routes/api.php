b4599b3b12261a19265dd90db469c25981d4b822
Jon <jon@dbs.org>
2018-10-02 14:28:17 -0500

    Added playlist to VideoStreamController index route

--- routes/api.php
+++ routes/api.php
@@ -50,7 +50,7 @@
 	Route::name('v4_access_groups.destroy')->delete('access/groups/{group_id}',                                         'User\AccessGroupController@destroy');
 
 	Route::name('v4_bible_filesets.types')->match(['get','options'],'bibles/filesets/media/types',                          'Bible\BibleFileSetsController@mediaTypes');
-	Route::name('v4_video_stream')->match(['get','options'], 'bible/filesets/{fileset_id}/stream/{file_id}',                'Bible\VideoStreamController@index');
+	Route::name('v4_video_stream')->match(['get','options'], 'bible/filesets/{fileset_id}/stream/{file_id}/playlist',       'Bible\VideoStreamController@index');
 	Route::name('v4_video_stream_ts')->match(['get','options'], 'bible/filesets/{fileset_id}/stream/{file_id}/{file_name}', 'Bible\VideoStreamController@transportStream');
 	Route::name('v4_bible_filesets.podcast')->match(['get','options'],'bibles/filesets/{fileset_id}/podcast',           'Bible\BibleFileSetsController@podcast');
 	Route::name('v4_bible_filesets.download')->match(['get','options'],'bibles/filesets/{fileset_id}/download',         'Bible\BibleFileSetsController@download');
