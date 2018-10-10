887d9ab0cccb0c53d25227cefa7ad3776ec831c3
Jon <jon@dbs.org>
2018-10-10 14:34:50 -0500

    Bible Links Additional Paramters

--- app/Http/Controllers/Bible/BibleLinksController.php
+++ app/Http/Controllers/Bible/BibleLinksController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Bible;
 
+use App\Models\Language\Language;
 use App\Transformers\BibleLinksTransformer;
 use Illuminate\Http\Request;
 use App\Models\Bible\BibleLink;
@@ -12,19 +13,26 @@ class BibleLinksController extends APIController
 {
     public function index()
     {
-	    $iso             = checkParam('iso', null, 'optional');
-	    $limit           = checkParam('limit', null, 'optional') ?? 25;
+	    $iso             = checkParam('iso', null, 'optional') ?? 'eng';
+	    $limit           = checkParam('limit', null, 'optional');
 	    $organization    = checkParam('organization_id', null, 'optional');
+	    $bible_id        = checkParam('bible_id', null, 'optional');
 
-	    if(isset($organization)) {
+	    if($organization !== null) {
 		    $organization = Organization::where('id',$organization)->orWhere('slug',$organization)->first();
-		    if(!$organization) return $this->setStatusCode(404)->replyWithError("organization not found");
+		    if(!$organization) return $this->setStatusCode(404)->replyWithError('organization not found');
 	    }
-
-	    $bibleLinks = BibleLink::with('bible.currentTranslation')->when($iso, function ($q) use ($iso) {
-		    $q->where('iso', $iso);
-	    })->when($organization, function ($q) use ($organization) {
-		    $q->where('organization_id', $organization->id);
+	    $language = Language::where('iso',$iso)->first();
+	    $bibleLinks = \DB::table(env('DBP_DATABASE').'.bible_links')
+		->join(env('DBP_DATABASE').'.bible_translations', function($q) use($language) {
+			$q->on('bible_links.bible_id','bible_translations.bible_id')->where('language_id',$language->id);
+		})
+		->when($organization, function ($q) use ($organization) {
+		    $q->where('bible_links.organization_id', $organization->id);
+	    })->when($limit, function ($q) use ($limit) {
+		    $q->limit($limit);
+	    })->when($bible_id, function ($q) use ($bible_id) {
+		    $q->where(env('DBP_DATABASE').'.bible_links.bible_id',$bible_id);
 	    })->where('visible',1)->get();
 
 	    return $this->reply(fractal($bibleLinks, new BibleLinksTransformer()));
