6c6c91653de3837ac85ed21b8d84ff64d80e6ef4
Jon <jon@dbs.org>
2018-10-12 11:49:06 -0500

    Created Custom Serializers for v1

---
+++ app/Transformers/Serializers/HighlightArraySerializer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Transformers\Serializers;
+
+use Spatie\Fractalistic\ArraySerializer;
+
+class HighlightArraySerializer extends ArraySerializer
+{
+	/**
+	 * Serialize a collection.
+	 *
+	 * @param string $resourceKey
+	 * @param array  $data
+	 *
+	 * @return array
+	 */
+	public function collection($resourceKey, array $data)
+	{
+		return ['highlights' => $data];
+	}
+
+	/**
+	 * Serialize an item.
+	 *
+	 * @param string $resourceKey
+	 * @param array  $data
+	 *
+	 * @return array
+	 */
+	public function item($resourceKey, array $data)
+	{
+		return ['highlights' => $data];
+	}
+
+	/**
+	 * Serialize null resource.
+	 *
+	 * @return array
+	 */
+	public function null()
+	{
+		return ['highlights' => []];
+	}
+}
---
+++ app/Transformers/Serializers/NoteArraySerializer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Transformers\Serializers;
+
+use Spatie\Fractalistic\ArraySerializer;
+
+class NoteArraySerializer extends ArraySerializer
+{
+	/**
+	 * Serialize a collection.
+	 *
+	 * @param string $resourceKey
+	 * @param array  $data
+	 *
+	 * @return array
+	 */
+	public function collection($resourceKey, array $data)
+	{
+		return ['notes' => $data];
+	}
+
+	/**
+	 * Serialize an item.
+	 *
+	 * @param string $resourceKey
+	 * @param array  $data
+	 *
+	 * @return array
+	 */
+	public function item($resourceKey, array $data)
+	{
+		return ['notes' => $data];
+	}
+
+	/**
+	 * Serialize null resource.
+	 *
+	 * @return array
+	 */
+	public function null()
+	{
+		return ['notes' => []];
+	}
+}
