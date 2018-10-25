e37c48c20403359046491ab33f00d2da96934be2
Jon <jon@dbs.org>
2018-10-25 14:15:12 -0500

    Add additional headers to preflight OPTIONS

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,12 +15,15 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-	    if ($request->getMethod() == "OPTIONS") {
-		    return response(['OK'], 200)->withHeaders([
-			    'Access-Control-Allow-Origin' => '*',
-			    'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE',
+	    if ($request->getMethod() === 'OPTIONS') {
+		    return response(['OK'], 204)->withHeaders([
+			    'Access-Control-Allow-Origin'      => '*',
+			    'Access-Control-Allow-Methods'     => 'GET, POST, PUT, OPTIONS, DELETE',
 			    'Access-Control-Allow-Credentials' => true,
-			    'Access-Control-Allow-Headers' => '*',
+			    'Access-Control-Allow-Headers'     => 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range',
+			    'Access-Control-Max-Age'           => 1728000,
+			    'Content-type'                     => 'text/plain; charset=utf-8',
+			    'Content-Length'                   => 0
 		    ]);
 	    }
 
