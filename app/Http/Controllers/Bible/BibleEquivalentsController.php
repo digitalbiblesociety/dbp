be3dfaf4f456391ea58394f073da9312ca778582
Jon <jon@dbs.org>
2018-11-06 13:51:36 -0600

    Add Multiple Responses to Equivalents Docs

--- app/Http/Controllers/Bible/BibleEquivalentsController.php
+++ app/Http/Controllers/Bible/BibleEquivalentsController.php
@@ -9,10 +9,7 @@ use Illuminate\Http\Response;
 class BibleEquivalentsController extends APIController
 {
 
-
 	/**
-	 *
-	 * Get the list of equivalents filtered by type or organization
 	 *
 	 * @link https://api.dbp.test/bibles/equivalents?key=1234&v=4&pretty
 	 *
@@ -20,17 +17,18 @@ class BibleEquivalentsController extends APIController
 	 *     path="/bible/equivalents",
 	 *     tags={"Bibles"},
 	 *     summary="Get a list of bible equivalents",
-	 *     description="Get a list of bible equivalents filtered by type or organization",
+	 *     description="Fetch a list of bible equivalents filtered by Type, Organization, or Bible",
 	 *     operationId="v4_bible.equivalents",
 	 *     @OA\Parameter(ref="#/components/parameters/version_number"),
 	 *     @OA\Parameter(ref="#/components/parameters/key"),
 	 *     @OA\Parameter(ref="#/components/parameters/pretty"),
 	 *     @OA\Parameter(ref="#/components/parameters/format"),
-	 *     @OA\Parameter(name="id", in="path", description="The abbreviated `Bible` id", required=true, @OA\Schema(ref="#/components/schemas/Bible/properties/id")),
 	 *     @OA\Response(
 	 *         response=200,
 	 *         description="successful operation",
-	 *         @OA\MediaType(mediaType="application/json", @OA\Schema(ref="#/components/schemas/BibleEquivalent"))
+	 *         @OA\MediaType(mediaType="application/json", @OA\Schema(ref="#/components/schemas/BibleEquivalent")),
+	 *         @OA\MediaType(mediaType="application/xml",  @OA\Schema(ref="#/components/schemas/BibleEquivalent")),
+	 *         @OA\MediaType(mediaType="text/x-yaml",      @OA\Schema(ref="#/components/schemas/BibleEquivalent"))
 	 *     )
 	 * )
 	 *
@@ -38,18 +36,21 @@ class BibleEquivalentsController extends APIController
 	 */
 	public function index()
 	{
-		$type     = checkParam('type',null,'optional');
-		$org_id   = checkParam('organization_id',null,'optional');
-		$bible_id = checkParam('bible_id',null,'optional');
+		// Check Params
+		$type     = checkParam('type', null, 'optional');
+		$org_id   = checkParam('organization_id', null, 'optional');
+		$bible_id = checkParam('bible_id', null, 'optional');
 
+		// Fetch Bible Equivalents
 		$bible_equivalents = BibleEquivalent::when($type, function ($q) use ($type) {
 			$q->where('type', $type);
 		})->when($org_id, function ($q) use ($org_id) {
-			$q->where('type', $org_id);
+			$q->where('organization_id', $org_id);
 		})->when($bible_id, function ($q) use ($bible_id) {
 			$q->where('bible_id', $bible_id);
 		})->get();
 
+		if(!$bible_equivalents) return $this->setStatusCode(404)->replyWithError(trans('api.bible_equivalents_errors_404'));
 		return $this->reply($bible_equivalents);
 	}
 
@@ -57,7 +58,7 @@ class BibleEquivalentsController extends APIController
 	 *
 	 * Get the list of equivalents for a single bible
 	 *
-	 * @link https://api.dbp.test/bibles/AAKWBT/equivalents?key=1234&v=4&pretty
+	 * @link https://api.dbp.test/bibles/AAKWBT/equivalents?key=TEST_KEY&v=4&pretty
 	 *
 	 * @OA\Get(
 	 *     path="/bibles/{id]/equivalents",
@@ -73,17 +74,19 @@ class BibleEquivalentsController extends APIController
 	 *     @OA\Response(
 	 *         response=200,
 	 *         description="successful operation",
-	 *         @OA\MediaType(mediaType="application/json", @OA\Schema(ref="#/components/schemas/BibleEquivalent"))
+	 *         @OA\MediaType(mediaType="application/json", @OA\Schema(ref="#/components/schemas/BibleEquivalent")),
+	 *         @OA\MediaType(mediaType="application/xml",  @OA\Schema(ref="#/components/schemas/BibleEquivalent")),
+	 *         @OA\MediaType(mediaType="text/x-yaml",      @OA\Schema(ref="#/components/schemas/BibleEquivalent"))
 	 *     )
 	 * )
 	 *
-	 * @param string $id
-	 *
+	 * @param $id
 	 * @return Response
 	 */
-    public function show(string $id)
+    public function show($id)
     {
-    	$bible_equivalents = BibleEquivalent::where('bible_id',$id)->get();
-    	return $this->reply($bible_equivalents);
+    	$bible_equivalent = BibleEquivalent::where('bible_id',$id)->get();
+    	if(!$bible_equivalent) return $this->setStatusCode(404)->replyWithError(trans('api.bible_equivalents_errors_404'));
+    	return $this->reply($bible_equivalent);
     }
 }
