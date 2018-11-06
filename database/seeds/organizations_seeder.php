2280a1dda51004acce466e9d860819720106ea05
Jon <jon@dbs.org>
2018-11-06 17:02:55 -0600

    Organization Seeder Updated

--- database/seeds/organizations_seeder.php
+++ database/seeds/organizations_seeder.php
@@ -21,34 +21,33 @@ class organizations_seeder extends Seeder
 		DB::connection('dbp')->table('organization_logos')->delete();
 		DB::connection('dbp')->table('organization_translations')->delete();
 		DB::connection('dbp')->table('organizations')->delete();
-		$organizations = $seederHelper->csv_to_array("https://docs.google.com/spreadsheets/d/".$googleSheetID."/export?format=csv&id=".$googleSheetID."&gid=0");
+		$organizations = $seederHelper->csv_to_array("https://docs.google.com/spreadsheets/d/$googleSheetID/export?format=csv&id=$googleSheetID&gid=0");
 		foreach ($organizations as $key => $data) {
 			$organization = new Organization();
-			foreach ($data as $key => $value) if($value == '') $data[$key] = null;
+			foreach($data as $subkey => $value) {
+				if($value === '') $data[$subkey] = null;
+			}
 			if(!$data['slug']) {continue;}
-			unset($data['fobai']);
-			unset($data['vernacularTitle']);
-			unset($data['dba']);
-			unset($data['globalContributor']);
-			unset($data['libraryContributor']);
-			unset($data['organization']);
-			unset($data['country']);
+			unset($data['fobai'], $data['vernacularTitle'], $data['dba'], $data['globalContributor'], $data['libraryContributor'], $data['organization'], $data['country']);
 
 			$organization->create($data);
 		}
 
+		$languages = \App\Models\Language\Language::where('iso','eng')->orWhere('iso','nep')->orWhere('iso','spa')->first();
 		$organizationTranslations = $seederHelper->csv_to_array("https://docs.google.com/spreadsheets/d/$googleSheetID/export?format=csv&id=$googleSheetID&gid=557153729");
 		foreach($organizationTranslations as $key => $data) {
+			$language = $languages->where('iso',$data['language_iso'])->first();
 			$organizationTranslation = new OrganizationTranslation();
-			$data['vernacular'] = boolval($data['vernacular']);
-			if($data['alt'] == '') $data['alt'] = 0;
+			$data['vernacular'] = (bool) $data['vernacular'];
+			$data['language_id'] = $language->id;
+			if($data['alt'] === '') $data['alt'] = 0;
 			$organizationTranslation->create($data);
 		}
 
 		$organizationLogos = $seederHelper->csv_to_array("https://docs.google.com/spreadsheets/d/$googleSheetID/export?format=csv&id=$googleSheetID&gid=1154991446");
 		foreach($organizationLogos as $key => $data) {
 			$organizationLogo = new OrganizationLogo();
-			if($data['url'] == '') { continue; }
+			if($data['url'] === '') { continue; }
 			$organizationLogo->create($data);
 		}
 
@@ -57,12 +56,12 @@ class organizations_seeder extends Seeder
 		foreach($dbpOrgs as $dbpOrg) {
 			foreach($organizations as $organization) {
 				// Organization
-				$translationExists = $organization->translations("eng")->first();
+				$translationExists = $organization->translations('eng')->first();
 				if($translationExists) {
 					$name = $translationExists->name;
-					if(($name == $dbpOrg['english_description']) or ($name == $dbpOrg['english_name']) or ($name == $dbpOrg['name'])) {
+					if(($name === $dbpOrg['english_description']) && ($name === $dbpOrg['english_name']) && ($name === $dbpOrg['name'])) {
 						// Organization
-						if($dbpOrg['donation_url'] != "NULL") {
+						if($dbpOrg['donation_url'] != 'NULL') {
 							$organization->url_donate = $dbpOrg['donation_url'];
 							$organization->save();
 						}
