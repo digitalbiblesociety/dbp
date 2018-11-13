686ce47a5e1fb373f6c13d53b29adf533fba35d3
Jon <jon@dbs.org>
2018-11-13 11:54:25 -0600

    Use static swagger json for testing

--- tests/Feature/API_V4_Test.php
+++ tests/Feature/API_V4_Test.php
@@ -17,14 +17,12 @@ class API_V4_Test extends TestCase
 	public function setUp()
 	{
 		parent::setUp();
-		config(['app.url' => 'https://api.dbp.test']);
-
 		$this->key    = '1234';
 		$this->params = ['v' => 4, 'key' => $this->key, 'pretty'];
 
 		// Fetch the Swagger Docs for Structure Validation
 		$arrContextOptions = ['ssl' => ['verify_peer' => false, 'verify_peer_name' => false]];
-		$swagger_url       = 'https://dbp.test/swagger_docs?v=v4';
+		$swagger_url       = base_path('resources/assets/js/swagger_v4.json');
 		$this->swagger     = json_decode(file_get_contents($swagger_url, false, stream_context_create($arrContextOptions)), true);
 		$this->schemas     = $this->swagger['components']['schemas'];
 		ini_set('memory_limit', '1264M');
