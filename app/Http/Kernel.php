6c2570ed71594dd0163a02528f68c4086fdffcbf
Jon <jon@dbs.org>
2018-11-15 15:59:38 -0600

    Enable Rate Limiting

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,11 +61,10 @@ class Kernel extends HttpKernel
             ShareErrorsFromSession::class,
 	        SubstituteBindings::class,
             VerifyCsrfToken::class,
-	        //Laravel2step::class,
 	        LocalizationHandler::class,
         ],
         'api' => [
-            //'throttle:120,1',
+            'throttle:120,1',
             'bindings',
 	        Cors::class
         ],
@@ -80,17 +79,31 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth'          => Authenticate::class,
-        'auth.basic'    => AuthenticateWithBasicAuth::class,
-        'bindings'      => SubstituteBindings::class,
-        'cache.headers' => SetCacheHeaders::class,
-        'can'           => Authorize::class,
-        'guest'         => RedirectIfAuthenticated::class,
-        'throttle'      => ThrottleRequests::class,
-        'role'          => VerifyRole::class,
-        'permission'    => VerifyPermission::class,
-        'level'         => VerifyLevel::class,
-        'currentUser'   => CheckCurrentUser::class,
-        'activated'     => CheckIsUserActivated::class,
+	    'auth' => \App\Http\Middleware\Authenticate::class,
+	    'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+	    'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+	    'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+	    'can' => \Illuminate\Auth\Middleware\Authorize::class,
+	    'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+	    'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+	    'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+	    'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+
+	/**
+	 * The priority-sorted list of middleware.
+	 *
+	 * This forces the listed middleware to always be in the given order.
+	 *
+	 * @var array
+	 */
+	protected $middlewarePriority = [
+		\Illuminate\Session\Middleware\StartSession::class,
+		\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+		\App\Http\Middleware\Authenticate::class,
+		\Illuminate\Session\Middleware\AuthenticateSession::class,
+		\Illuminate\Routing\Middleware\SubstituteBindings::class,
+		\Illuminate\Auth\Middleware\Authorize::class,
+	];
+
 }
