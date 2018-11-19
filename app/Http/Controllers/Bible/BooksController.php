f36acb15da66f4622d44c4f5abdb70ee358f5e58
Jon <jon@dbs.org>
2018-11-19 17:10:11 -0600

    Refactored BooksController

--- app/Http/Controllers/Bible/BooksController.php
+++ app/Http/Controllers/Bible/BooksController.php
@@ -7,6 +7,7 @@ use App\Models\Bible\BibleFileset;
 use App\Transformers\BooksTransformer;
 use App\Http\Controllers\APIController;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class BooksController extends APIController
 {
@@ -110,44 +111,25 @@ class BooksController extends APIController
 
         $cache_string = 'bible_books_'.$id.$fileset_type.$asset_id;
         $books = \Cache::remember($cache_string, 2400, function () use ($fileset_type, $asset_id, $id) {
-
             $is_plain_text = \Schema::connection('sophia')->hasTable($id . '_vpl');
-            $filesetExists = BibleFileset::uniqueFileset($id, $asset_id, $fileset_type)->first();
-            if (!$filesetExists) {
+            $fileset = BibleFileset::uniqueFileset($id, $asset_id, $fileset_type)->first();
+
+            if (!$fileset) {
                 return $this->replyWithError('Fileset Not Found');
             }
 
-            $books = \DB::connection('dbp')->table('dbp.bible_filesets as fileset')
+            $dbp_database = config('database.connections.dbp.database');
+            $books = \DB::connection('dbp')->table($dbp_database.'.bible_filesets as fileset')
                 ->where('fileset.id', $id)->where('fileset.asset_id', $asset_id)
-                ->leftJoin('dbp.bible_fileset_connections as connection', 'connection.hash_id', 'fileset.hash_id')
-                ->leftJoin('dbp.bibles', 'bibles.id', 'connection.bible_id')
+                ->leftJoin($dbp_database.'.bible_fileset_connections as connection', 'connection.hash_id', 'fileset.hash_id')
+                ->leftJoin($dbp_database.'.bibles', 'bibles.id', 'connection.bible_id')
                 ->when($fileset_type, function ($q) use ($fileset_type) {
                     $q->where('set_type_code', $fileset_type);
                 })
-                ->when($is_plain_text, function ($q) use ($id) {
-
-                    // If the fileset references sophia.*_vpl than fetch the existing books from that database
-                    $sophia_books = \DB::connection('sophia')->table($id . '_vpl')
-                        ->join('dbp.books', 'books.id_usfx', $id . '_vpl.book')
-                        ->select('books.id')->distinct()->get()->pluck('id');
-
-                    // Join the books for the books returned from Sophia
-                    $q->join('dbp.bible_books', function ($join) use ($sophia_books) {
-                        $join->on('bible_books.bible_id', 'bibles.id')
-                             ->whereIn('bible_books.book_id', $sophia_books);
-                    })->rightJoin('dbp.books', 'books.id', 'bible_books.book_id');
-                }, function ($q) use ($filesetExists) {
-
-                    // If the fileset references dbp.bible_files from that table
-                    $files_books = \DB::connection('dbp')->table('bible_files')
-                        ->where('hash_id', $filesetExists->hash_id)->select(['book_id'])
-                        ->distinct()->get()->pluck('book_id');
-
-                    // Join the books for the books returned from bible_files
-                    $q->join('dbp.bible_books', function ($join) use ($files_books) {
-                        $join->on('bible_books.bible_id', 'bibles.id')
-                             ->whereIn('bible_books.book_id', $files_books);
-                    })->rightJoin('dbp.books', 'books.id', 'bible_books.book_id');
+                ->when($is_plain_text, function ($query) use ($id) {
+                    $this->compareFilesetToSophiaBooks($query, $id);
+                }, function ($query) use ($fileset) {
+                    $this->compareFilesetToFileTableBooks($query, $fileset->hash_id);
                 })
                 ->orderBy('books.protestant_order')->select([
                     'books.id',
@@ -169,5 +151,49 @@ class BooksController extends APIController
         }
 
         return $this->reply($books);
+    }
+
+    /**
+     *
+     * @param $query
+     * @param $id
+     */
+    private function compareFilesetToSophiaBooks($query, $id)
+    {
+        // If the fileset references sophia.*_vpl than fetch the existing books from that database
+        $dbp_database = config('database.connections.dbp.database');
+        $sophia_books = DB::connection('sophia')->table($id . '_vpl')
+                           ->join('dbp.books', 'books.id_usfx', $id . '_vpl.book')
+                           ->select('books.id')->distinct()->get()->pluck('id');
+
+        // Join the books for the books returned from Sophia
+        $query->join($dbp_database.'.bible_books', function ($join) use ($sophia_books) {
+            $join->on('bible_books.bible_id', 'bibles.id')
+                 ->whereIn('bible_books.book_id', $sophia_books);
+        })->rightJoin('dbp.books', 'books.id', 'bible_books.book_id');
+    }
+
+    /**
+     *
+     * @param $query
+     * @param $hashId
+     */
+    private function compareFilesetToFileTableBooks($query, $hashId)
+    {
+        // If the fileset referencesade dbp.bible_files from that table
+        $dbp_database = config('database.connections.dbp.database');
+        $fileset_book_ids = DB::connection('dbp')
+            ->table('bible_files')
+            ->where('hash_id', $hashId)
+            ->select(['book_id'])
+            ->distinct()
+            ->get()
+            ->pluck('book_id');
+
+        // Join the books for the books returned from bible_files
+        $query->join($dbp_database.'.bible_books', function ($join) use ($fileset_book_ids) {
+            $join->on('bible_books.bible_id', 'bibles.id')
+                 ->whereIn('bible_books.book_id', $fileset_book_ids);
+        })->rightJoin('dbp.books', 'books.id', 'bible_books.book_id');
     }
 }
