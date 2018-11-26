e109e4ed05fdad702e0ffda6c586d511ab12ed39
Jon <jon@dbs.org>
2018-11-26 11:52:07 -0600

    added project_id to fillable array

--- app/Models/User/Account.php
+++ app/Models/User/Account.php
@@ -20,7 +20,7 @@ class Account extends Model
 {
     protected $connection = 'dbp_users';
     protected $table = 'user_accounts';
-    protected $fillable = ['user_id', 'provider_user_id', 'provider_id','created_at'];
+    protected $fillable = ['user_id', 'provider_user_id', 'provider_id', 'project_id'];
     protected $hidden = ['user_id'];
 
     /**
