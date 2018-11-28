2ac80c02231f98168773e86ba69b8f1354b041ae
Jon <jon@dbs.org>
2018-11-28 17:48:16 -0600

    Library Version Optimized

--- app/Http/Controllers/Connections/V2Controllers/LibraryCatalog/LibraryVersionController.php
+++ app/Http/Controllers/Connections/V2Controllers/LibraryCatalog/LibraryVersionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Connections\V2Controllers\LibraryCatalog;
 
 use App\Http\Controllers\APIController;
+use App\Models\Language\Language;
 use Illuminate\Http\Request;
 use App\Models\Bible\BibleFileset;
 use App\Transformers\V2\LibraryCatalog\LibraryCatalogTransformer;
@@ -14,25 +15,36 @@ class LibraryVersionController extends APIController
      *
      * Get the list of versions defined in the system
      *
-     * @link https://api.dbp.test/library/version?key=1234&v=2
-     *
-     * @param code (optional): Get the entry for a three letter version code.
-     * @param name (optional): Get the entry for a part of a version name in either native language or English.
-     * @param sort_by (optional): [code|name|english] Primary criteria by which to sort. 'name' refers to the native language name. The default is 'english'.
-     *
      * @OA\Get(
      *     path="/library/version",
      *     tags={"Library Catalog"},
      *     summary="Returns Audio File path information",
-     *     description="This call returns the file path information for audio files for a volume. This information can be used with the response of the /audio/location call to create a URI to retrieve the audio files.",
+     *     description="This call returns the file path information for audio files for a volume. This information can
+               be used with the response of the /audio/location call to create a URI to retrieve the audio files.",
      *     operationId="v2_library_version",
      *     @OA\Parameter(ref="#/components/parameters/version_number"),
      *     @OA\Parameter(ref="#/components/parameters/key"),
      *     @OA\Parameter(ref="#/components/parameters/pretty"),
      *     @OA\Parameter(ref="#/components/parameters/format"),
-     *     @OA\Parameter(name="code", in="query", description="The abbreviated `BibleFileset` id created from the three letters identifier after the iso code", required=true, @OA\Schema(ref="#/components/schemas/BibleFileset/properties/id")),
-     *     @OA\Parameter(name="name", in="query", description="The name of the version in the language that it's written in", @OA\Schema(ref="#/components/schemas/BibleFile/properties/chapter_start")),
-     *     @OA\Parameter(name="sort_by", in="query", description="The name of the version in english", @OA\Schema(type="string",title="encoding")),
+     *     @OA\Parameter(
+     *         name="code",
+     *         in="query",
+     *         @OA\Schema(ref="#/components/schemas/BibleFileset/properties/id"),
+     *         description="The abbreviated `BibleFileset` id created from the letters after the iso",
+     *         required=true,
+     *     ),
+     *     @OA\Parameter(
+     *         name="name",
+     *         in="query",
+     *         @OA\Schema(ref="#/components/schemas/BibleFile/properties/chapter_start"),
+     *         description="The name of the version in the language that it's written in"
+     *     ),
+     *     @OA\Parameter(
+     *         name="sort_by",
+     *         in="query",
+     *         @OA\Schema(type="string",title="encoding"),
+     *         description="The name of the version in english"
+     *     ),
      *     @OA\Response(
      *         response=200,
      *         description="successful operation",
@@ -46,9 +58,18 @@ class LibraryVersionController extends APIController
      *     description="The various version ids in the old version 2 style",
      *     title="v2_library_version",
      *     @OA\Xml(name="v2_library_version"),
-     *     @OA\Property(property="version_code",type="string",description="The abbreviated `BibleFileset` id created from the three letters identifier after the iso code"),
-     *     @OA\Property(property="version_name",type="string",description="The name of the version in the language that it's written in"),
-     *     @OA\Property(property="english_name",type="string",description="The name of the version in english")
+     *     @OA\Property(
+     *         property="version_code",
+     *         type="string",
+     *         description="The abbreviated `BibleFileset` id created from letters after the iso code"),
+     *     @OA\Property(
+     *         property="version_name",
+     *         type="string",
+     *         description="The name of the version in the language that it's written in"),
+     *     @OA\Property(
+     *         property="english_name",
+     *         type="string",
+     *         description="The name of the version in english")
      * )
      *
      * @return json
@@ -59,14 +80,27 @@ class LibraryVersionController extends APIController
         $name = checkParam('name');
         $sort = checkParam('sort_by');
 
-        $versions = BibleFileset::where('asset_id', config('filesystems.disks.s3_fcbh.bucket'))
-            ->with('bible.currentTranslation', 'bible.vernacularTranslation')
-            ->when($code, function ($q) use ($code) {
-                $q->where('id', $code);
-            })->when($sort, function ($q) use ($sort) {
-                $q->orderBy($sort);
-            })->get();
+        $versions = \Cache::remember('libraryVersion'.$code.$name.$sort,2800, function() use($code, $sort, $name) {
 
-        return $this->reply(fractal()->collection($versions)->transformWith(LibraryCatalogTransformer::class)->serializeWith($this->serializer));
+            $versions = BibleFileset::where('asset_id', config('filesystems.disks.s3_fcbh.bucket'))
+                ->with('bible.translations')
+                ->when($code, function ($q) use ($code) {
+                    $q->where('id', $code);
+                })->when($sort, function ($q) use ($sort) {
+                    $q->orderBy($sort,'asc');
+                })
+                ->when($name, function ($query) use ($name) {
+                    $query->whereHas('bible.translations', function($sub_query) use ($name) {
+                        $sub_query->where('name',$name);
+                    });
+                })->get();
+
+            $language = Language::where('iso','eng')->select('id')->first();
+            $versions->map->eng_id = $language->id;
+
+            return fractal($versions, LibraryCatalogTransformer::class, $this->serializer);
+        });
+
+        return $this->reply($versions);
     }
 }
--- app/Transformers/V2/LibraryCatalog/LibraryCatalogTransformer.php
+++ app/Transformers/V2/LibraryCatalog/LibraryCatalogTransformer.php
@@ -9,10 +9,13 @@ class LibraryCatalogTransformer extends BaseTransformer
 
     public function transform($version)
     {
+        $translations = optional($version->bible->first())->translations;
+        $vernacular = $translations ? optional($translations->where('vernacular',1)->first())->name : '';
+        $english = $translations ? optional($translations->where('language_id',$version->eng_id)->first())->name : '';
         return [
-            'version_code' => $version->id,
-            'version_name' => $version->bible->first()->vernacularTranslation->name ?? '',
-            'english_name' => $version->bible->first()->currentTranslation->name ?? ''
+            'version_code' => substr($version->id,3),
+            'version_name' => (string) $vernacular,
+            'english_name' => (string) $english
         ];
     }
 }
