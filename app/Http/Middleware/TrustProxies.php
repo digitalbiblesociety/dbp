ae37564fb35e9a34e907a47bf79cdba4df8c6f46
Jon <jon@dbs.org>
2019-01-15 12:24:53 -0600

    Added Trusted Proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    protected $proxies = [
+        '172.31.0.0/16'
+    ];
 
     /**
      * The headers that should be used to detect proxies.
