fb8314d8920908598d3fefa513895c3f28ec4404
Jon <jon@dbs.org>
2019-01-22 11:54:03 -0600

    Added Enddate to sync v2

--- app/Console/Commands/syncV2.php
+++ app/Console/Commands/syncV2.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Bible\BibleVerse;
+use App\Models\Language\Language;
 use App\Models\User\Study\HighlightColor;
 use Illuminate\Console\Command;
 
@@ -28,7 +29,7 @@ class syncV2 extends Command
      *
      * @var string
      */
-    protected $signature = 'sync:v2 {action} {date?}';
+    protected $signature = 'sync:v2 {action} {date?} {end_date?}';
 
     /**
      * The console command description.
@@ -55,8 +56,11 @@ class syncV2 extends Command
     public function handle()
     {
 
-        $from_date = $this->argument('date');
+        $from_date = $this->argument('date') ?? '00-00-00';
         $from_date = Carbon::createFromFormat('Y-m-d', $from_date)->startOfDay();
+
+        $end_date = $this->argument('end_date') ?? '2020-01-01';
+        $end_date = Carbon::createFromFormat('Y-m-d', $end_date)->endOfDay();
 
         switch($this->argument('action')) {
             case 'users':
@@ -79,6 +83,26 @@ class syncV2 extends Command
 
     private function syncUsers($from_date)
     {
+        \DB::connection('dbp_users_v2')->table('user')->where('created', '>', $from_date)->orderBy('id')
+           ->chunk(50000, function ($users) {
+               foreach($users as $user) {
+                   User::firstOrCreate([
+                       'id'               => $user->id,
+                       'v2_id'            => $user->id,
+                       'name'             => $user->username ?? $user->email,
+                       'password'         => bcrypt($user->password),
+                       'first_name'       => $user->first_name,
+                       'last_name'        => $user->last_name,
+                       'token'            => str_random(24),
+                       'email'            => $user->email,
+                       'activated'        => (int) $user->confirmed,
+                       'created_at'       => Carbon::createFromTimeString($user->created),
+                       'updated_at'       => Carbon::createFromTimeString($user->updated),
+                   ]);
+               }
+           });
+
+        /*
         $newV2Users = \DB::connection('dbp_users_v2')->table('user')->where('created', '>', $from_date)->get();
         $newV2UsersEmails = $newV2Users->pluck('email');
         $matchingV4UserEmails = User::whereIn('email', $newV2UsersEmails)->pluck('email');
@@ -101,67 +125,90 @@ class syncV2 extends Command
             }
 
         }
+        */
     }
 
     private function syncHighlights($from_date)
     {
         $this->highlightColors = HighlightColor::select('color', 'id')->get()->pluck('id','color')->toArray();
-        $bibles = Bible::with('filesets')->get();
+        $filesets = BibleFileset::where('set_type_code','text_plain')->where('asset_id','dbp-prod')->get();
         $books = Book::select(['id_osis','id_usfx','id','protestant_order'])->get();
 
         \DB::connection('dbp_users_v2')
            ->table('highlight')
            ->where('created', '>', $from_date)
            ->orderBy('created')
-           ->chunk(500, function ($highlights) use($bibles, $books) {
+           ->chunk(10000, function ($highlights) use($filesets, $books) {
                foreach($highlights as $highlight) {
-                   $this->syncHighlight($highlight, $bibles, $books);
+                   $this->syncHighlight($highlight, $filesets, $books);
                }
            });
     }
 
-    private function syncHighlight($highlight, $bibles, $books)
+    private function syncHighlight($highlight, $filesets, $books)
     {
-        $bible = $bibles->where('id', substr($highlight->dam_id, 0, 6))->first();
-        if(!$bible) {
-            Log::driver('seed_errors')->info('bb_nfd_'.substr($highlight->dam_id,0,6));
-            return;
-        }
-        $fileset = $bible->filesets->where('set_type_code','text_plain')->first();
+        $fileset = $filesets->where('id',substr($highlight->dam_id,0,6))->first();
         if(!$fileset) {
+            Log::driver('seed_errors')->info('bb_nfd_'.$highlight->dam_id);
+            echo "\n Error!! Could not find FILESET_ID: ".substr($highlight->dam_id,0,6);
             return;
         }
         $book = $books->where('id_osis', $highlight->book_id)->first();
-        if($book === null) {
-            $book = $books->where('protestant_order',$highlight->book_id)->first();
+        if(!$book) {
+            $book = $books->where('protestant_order',$highlight->book_id);
+            echo "\n Error!! Could not find BOOK_ID: ".$highlight->book_id;
+            return;
         }
+
         if($book === null) {
-            Log::driver('seed_errors')->info('bb_nfd_'.$highlight->book_id);
+            Log::driver('seed_errors')->info('bb_nfb_'.$highlight->book_id);
+            echo "\n Error!! Could not find BOOK_ID: ".$highlight->book_id;
             return;
         }
 
         $user_exists = User::where('v2_id',$highlight->user_id)->first();
-        $verse = BibleVerse::where('hash_id',$fileset->hash_id)->select(['verse_text','chapter'])
-                           ->where('book_id',$book->id)->where('chapter',$highlight->chapter_id)
-                           ->where('verse_start',$highlight->verse_id)->first();
-
-        if ($verse === null) {
-            Log::driver('seed_errors')->info('bb_nfd_'.$fileset->id);
-            throw new \Exception('Not getting a verse for bb_nfd_' . $fileset->id);
+        if(!$user_exists) {
+            Log::driver('seed_errors')->info('bb_nfu_'.$highlight->user_id);
+            echo "\n Error!! Could not find USER_ID: ".$highlight->user_id;
+            return;
         }
 
         $v4Highlight = Highlight::firstOrNew([
             'user_id'           => $user_exists->id,
-            'bible_id'          => $bible->id,
+            'hash_id'          => $fileset->hash_id,
             'book_id'           => $book->id,
             'chapter'           => $highlight->chapter_id,
             'verse_start'       => $highlight->verse_id,
             'highlight_start'   => 1,
-            'highlighted_words' => substr_count($verse->verse_text, ' ') + 1,
+            'highlighted_chars' => NULL,
             'highlighted_color' => $this->getRelatedColorIdForHighlightColorString($highlight->color),
+            'created_at'        => Carbon::createFromTimeString($highlight->created),
+            'updated_at'        => Carbon::createFromTimeString($highlight->updated),
         ]);
         $v4Highlight->v2_id = $highlight->id;
         $v4Highlight->save();
+        echo "\n Highlight Processed: ". $highlight->id;
+    }
+
+
+    private function waitForUserSeeder($user_id)
+    {
+        $user_exists = User::where('v2_id',$user_id)->first();
+        while(!$user_exists) {
+            $v2_user = \DB::connection('dbp_users_v2')->table('user')->where('id',$user_id)->first();
+            $user_exists = User::where('email', $v2_user->email)->first();
+            if(isset($user_exists)) {
+                $user_exists->v2_id = $v2_user->id;
+                $user_exists->save();
+                echo "\nUser v2 id updated";
+            } else {
+                $skippUser ++;
+                echo "\nWaiting for users seeder for ID: $user_id";
+                sleep(15);
+                continue;
+            }
+        }
+        return $user_exists;
     }
 
     private function getRelatedColorIdForHighlightColorString($colorString)
@@ -188,17 +235,18 @@ class syncV2 extends Command
                 }
                 if(!isset($books[$note->book_id])) {continue;}
 
-                $note = Note::firstOrNew([
+                $note = Note::create([
+                    'v2_id'       => $note->id,
                     'user_id'     => $note->user_id,
                     'bible_id'    => $fileset->bible->first()->id,    //  => "ENGESVO2ET"
                     'book_id'     => $books[$note->book_id],          //  => "Ezra"
                     'chapter'     => $note->chapter_id,               //  => "1"
                     'verse_start' => $note->verse_id,                 //  => "1"
+                    'notes'       => encrypt($note->note),
                     'created_at'  => Carbon::createFromTimeString($note->created)->toDateString(),
                     'updated_at'  => Carbon::createFromTimeString($note->updated)->toDateString(),
                 ]);
-                $note->v2_id = $note->id;
-                $note->save();
+                echo "\n Note Processed: ". $note->id;
             }
         });
 
@@ -274,8 +322,9 @@ class syncV2 extends Command
     {
         $countries = Country::all();
         \DB::connection('dbp_users_v2')->table('user_profile')->where('field_value','!=','')
-            ->where('created', '>', $created_at)->orderBy('id')
-           ->chunk(500, function ($profiles) use($countries) {
+           ->where('created', '>', $created_at)
+           ->orderBy('id')
+           ->chunk(5000, function ($profiles) use($countries) {
                foreach ($profiles as $profile) {
                    $user_exists = User::where('v2_id',$profile->user_id)->first();
                    while(!$user_exists) {
@@ -331,7 +380,7 @@ class syncV2 extends Command
                        }
                        case 'phone_number':
                        case 'phone': {
-                           if(strlen($profile->field_value) > 22) {
+                           if(\strlen($profile->field_value) > 22) {
                                //echo "\nToo long for phone:".$profile->field_value;
                                break;
                            }
@@ -339,10 +388,11 @@ class syncV2 extends Command
                            break;
                        }
                        case 'locale': {
-                           if(strlen($profile->field_value) > 5) {
-                               //echo "\nToo long for locale:".$profile->field_value;
-                               break;
+                           $lang = Language::where('iso_2B',substr($profile->field_value,0,2))->select('id')->first();
+                           if($lang) {
+                               $current_profile->language_id = $lang->id;
                            }
+                           break;
                        }
                        default: {
                            $current_profile->{$profile->field_name} = $profile->field_value;
