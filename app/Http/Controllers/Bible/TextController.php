e8861fd2f80f50c691afa50c6b3c425c6d3b0078
Jon <jon@dbs.org>
2019-01-28 10:13:31 -0600

    Change cache String for bible_text

--- app/Http/Controllers/Bible/TextController.php
+++ app/Http/Controllers/Bible/TextController.php
@@ -100,7 +100,7 @@ class TextController extends APIController
             return $this->setStatusCode(403)->replyWithError('Your API Key does not have access to this fileset');
         }
 
-        $cache_string = strtolower('bible_text'.$fileset_id.$book_id.$chapter.$verse_start.$verse_end.$asset_id);
+        $cache_string = strtolower('bible_text:'.$asset_id.':'.$fileset_id.':'.$book_id.':'.$chapter.':'.$verse_start.':'.$verse_end);
         $verses = \Cache::remember($cache_string, 2400, function() use ($fileset,$bible,$book,$chapter,$verse_start,$verse_end) {
            return BibleVerse::withVernacularMetaData($bible)
                 ->where('hash_id', $fileset->hash_id)
