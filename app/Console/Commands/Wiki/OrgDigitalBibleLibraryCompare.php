0da53469e6e006a6547e7273b21a1df84ef00630
Jon Bitgood <jon@dbs.org>
2019-02-14 23:12:14 -0600

    Reduce dbl organization comparison complexity

--- app/Console/Commands/Wiki/OrgDigitalBibleLibraryCompare.php
+++ app/Console/Commands/Wiki/OrgDigitalBibleLibraryCompare.php
@@ -35,18 +35,12 @@ class OrgDigitalBibleLibraryCompare extends Command
     {
         $organizations = json_decode(file_get_contents(storage_path('data/organizations/organizations_dbl.json')));
         $dbl_id = Organization::where('slug', 'digital-bible-library')->first()->id;
-        $pre_matched = json_decode(file_get_contents('https://gist.githubusercontent.com/jonBitgood/500214188481e868cb3b06e87f33e06e/raw/ae4ca98978d04d5c0d6927003a3f4f55ceae699c/dbl-orgs-to-dbp-orgs.json'), true);
 
-        OrganizationRelationship::where('organization_parent_id',$dbl_id)->delete();
-        foreach ($pre_matched['data'] as $pre_matched_org) {
-            OrganizationRelationship::create($pre_matched_org);
-        }
+        $this->handlePreMatchedOrgs($dbl_id);
 
         foreach ($organizations->orgs as $dbl_organization) {
-            $duplicates = [
-                '55dfef9e5117ad36e0f362c9' // Bible Society of Sudan
-            ];
-            if(\in_array($dbl_organization->id,$duplicates)) {
+            $duplicates = ['55dfef9e5117ad36e0f362c9'];
+            if (\in_array($dbl_organization->id, $duplicates)) {
                 continue;
             }
 
@@ -58,52 +52,58 @@ class OrgDigitalBibleLibraryCompare extends Command
 
             // Second handle direct equivalents
             if ($translationMatchExists) {
-                $organization = $translationMatchExists->organization;
                 OrganizationRelationship::firstOrCreate([
                     'type'                   => 'Member',
-                    'organization_child_id'  => $organization->id,
+                    'organization_child_id'  => $translationMatchExists->organization->id,
                     'organization_parent_id' => $dbl_id,
                     'relationship_id'        => $dbl_organization->id
                 ]);
             } else {
-                // Otherwise Fuzzy Search for Provider Name
-                $organizations = Searchy::driver('ufuzzy')->search(config('database.connections.dbp.database').'.organization_translations')->fields('name')->query($dbl_organization->full_name)->getQuery()->limit(5)->get();
-                if(!isset($organizations)) {
-                    $missing[] = $dbl_organization->full_name;
-                    continue;
-                }
-                if ($organizations->count() === 0) {
-                    $missing[] = $dbl_organization->full_name;
-                    continue;
-                }
-
-                // Present Data to User
-                $this->comment("\n\n==========$dbl_organization->full_name==========");
-                $this->info(json_encode($organizations->pluck('name', 'organization_id'), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
-
-                // Get User Input
-                $confirmed       = false;
-                $organization_id = $this->ask('Please enter the number of the Closest Match, if none just hit 0');
-                if ((int) $organization_id === 0) {
-                    $missing[$dbl_organization->id] = $dbl_organization->full_name;
-                    continue;
-                }
-
-                if (!OrganizationRelationship::where([
-                    'type'                   => 'Member',
-                    'organization_child_id'  => $organization_id,
-                    'organization_parent_id' => $dbl_id,
-                    'relationship_id'        => $dbl_organization->id
-                ])->first()) {
-                    OrganizationRelationship::firstOrCreate([
-                        'type'                   => 'Member',
-                        'organization_child_id'  => $organization_id,
-                        'organization_parent_id' => $dbl_id,
-                        'relationship_id'        => $dbl_organization->id
-                    ]);
-                }
+                $this->fuzzySearchOrgs($dbl_id);
             }
         }
-        print_r($missing);
+    }
+
+    private function fuzzySearchOrgs($dbl_id)
+    {
+        // Otherwise Fuzzy Search for Provider Name
+        $organizations = Searchy::driver('ufuzzy')->search(config('database.connections.dbp.database').'.organization_translations')->fields('name')->query($dbl_organization->full_name)->getQuery()->limit(5)->get();
+        if (!isset($organizations)) {
+            $missing[] = $dbl_organization->full_name;
+            return false;
+        }
+        if ($organizations->count() === 0) {
+            $missing[] = $dbl_organization->full_name;
+            return false;
+        }
+
+        // Present Data to User
+        $this->comment("\n\n==========$dbl_organization->full_name==========");
+        $this->info(json_encode($organizations->pluck('name', 'organization_id'), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+        // Get User Input
+        $organization_id = $this->ask('Please enter the number of the Closest Match, if none just hit 0');
+        if ((int) $organization_id === 0) {
+            $missing[$dbl_organization->id] = $dbl_organization->full_name;
+            return false;
+        }
+
+        OrganizationRelationship::firstOrCreate([
+            'type'                   => 'Member',
+            'organization_child_id'  => $organization_id,
+            'organization_parent_id' => $dbl_id,
+            'relationship_id'        => $dbl_organization->id
+        ]);
+    }
+
+    private function handlePreMatchedOrgs($dbl_id)
+    {
+        OrganizationRelationship::where('organization_parent_id', $dbl_id)->delete();
+
+        $pre_matched_path = 'https://gist.githubusercontent.com/jonBitgood/500214188481e868cb3b06e87f33e06e/raw/ae4ca98978d04d5c0d6927003a3f4f55ceae699c/dbl-orgs-to-dbp-orgs.json';
+        $pre_matched = json_decode(file_get_contents($pre_matched_path), true);
+        foreach ($pre_matched['data'] as $pre_matched_org) {
+            OrganizationRelationship::create($pre_matched_org);
+        }
     }
 }
