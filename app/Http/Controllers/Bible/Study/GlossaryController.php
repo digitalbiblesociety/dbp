9b383490f56793d0f12c132260951c6ecfac3fb3
Jon Bitgood <jon@dbs.org>
2019-02-26 16:21:39 -0600

    Create GlossaryController.php

---
+++ app/Http/Controllers/Bible/Study/GlossaryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Bible\Study;
+
+use App\Models\Bible\Study\GlossaryPerson;
+use Illuminate\Http\Request;
+
+class GlossaryController extends APIController
+{
+
+    public function people()
+    {
+        $glossary_person = checkParam('glossary_person');
+
+        $person = GlossaryPerson::with('names', 'relationships', 'translation')->where('id',$glossary_person)->first();
+        return $person;
+    }
+
+    // public function
+
+
+
+}
