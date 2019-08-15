bd2443d5b953bf2ba8ea26bd9015d9ca6af55acd
Mauricio Gonzalez Salcedo <mgonzalez@fullstacklabs.co>
2019-08-14 19:11:00 -0500

    FCBH-406 PR Comments
    - Changed the order the playlists to be by name
    - Added following parameter to playlist GET endpoint

--- app/Models/Playlist/Playlist.php
+++ app/Models/Playlist/Playlist.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Carbon\Carbon;
 use App\Models\User\User;
+use FollowPlaylists;
 
 /**
  * App\Models\Playlist
