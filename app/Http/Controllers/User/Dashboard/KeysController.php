b0b0688af156f10168b75b953d7e816ed9a6bf32
Adam Burdette <adam@fullstacklabs.co>
2019-11-07 18:37:26 -0500

    FCBH-605 Add path to plan days
    Added the hls path to the plan day items.

--- app/Http/Controllers/User/Dashboard/KeysController.php
+++ app/Http/Controllers/User/Dashboard/KeysController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\User\Dashboard;
 
 use App\Http\Controllers\APIController;
 use App\Models\User\Key;
-use App\Models\User\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
