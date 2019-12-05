daa05648c5ef5daef2875366394a68408113ca37
Adam Burdette <adam@fullstacklabs.co>
2019-12-05 09:29:34 -0500

    FCBH-955 Alter return of fonts data getter

--- app/Transformers/BibleTransformer.php
+++ app/Transformers/BibleTransformer.php
@@ -248,7 +248,7 @@ class BibleTransformer extends BaseTransformer
                     'fonts' => $bible->filesets->reduce(function ($carry, $item) {
                         foreach ($item->fonts as $font) {
                             if (!isset($carry[$font->name])) {
-                                $carry[$font->name] = ['data' => $font->data, 'type' => $font->type];
+                                $carry = ['name' => $font->name, 'data' => $font->data, 'type' => $font->type];
                             }
                         }
                         return $carry;
